3056b4f26802998158debd9ba2a27c21d24d3005
RominaMalimban <romina.lyn@hotmail.it>
2022-11-10 16:45:54 +0100

    bozza css

--- gioco-dadi/js/script.js
+++ gioco-dadi/js/script.js
@@ -1,20 +1,34 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// creo variabile per bottone:
+let myButton = document.querySelector("button")
 
-// genero il mio numero:
-let myNum = Math.floor(Math.random() * 6) + 1;
-console.log("mia scelta ", myNum);
+// creo funzione al click:
+myButton.addEventListener("click",
 
-// genero il numero del computer:
-let numCpu = Math.floor(Math.random() * 6) + 1;
-console.log("scelta cpu ", numCpu);
+    function(){
+        // genero il mio numero:
+        let myNum = Math.floor(Math.random() * 6) + 1;
+        console.log("mia scelta ", myNum);
+        document.getElementById("myNum").innerHTML= myNum;
+       
+        // genero il numero del computer:
+        let numCpu = Math.floor(Math.random() * 6) + 1;
+        console.log("scelta cpu ", numCpu);
+        document.getElementById("numCpu").innerHTML= numCpu;
+        
 
-// creo le mie condizioni:
-if (myNum > numCpu){
-    console.log("Complimenti, hai vinto");
-}else if(myNum == numCpu){
-    console.log("Ops, partita patta");
-}else{
-    console.log("Mi dispiace, ha vinto il computer")
-}
+        // creo le mie condizioni:
+        if (myNum > numCpu){
+            document.querySelector("h3").innerHTML ="Complimenti, hai vinto!";
+        }else if(myNum == numCpu){
+            document.querySelector("h3").innerHTML ="Ops, partita patta! Ritenta!";
+        }else{
+            document.querySelector("h3").innerHTML ="Mi dispiace, hai perso!";
+        }
+    }
+    
+);
+
+
