dcb26b6831ec4b78f0b116f05f5cdfe65f52d957
RominaMalimban <romina.lyn@hotmail.it>
2022-11-10 14:54:31 +0100

    creazione condizione se user è invitato

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,14 +4,23 @@
 
 
 // chiedo all'utente la sua mail:
-let userMail = prompt("Per favore, inserisci la tua mail:")
+let userMail = prompt("Per favore, inserisci la tua mail:");
 console.log("email utente:", userMail);
 
 // creo lista delle mail degli invitati:
 let listaInvitati = ["romina@gmail.com", "pincopallo@gmail.com", "pippo@gmail.com"];
 
+for (let i = 0; i < listaInvitati.length; i++){
+    // console.log(listaInvitati[i]);
 
-// controllo che la mail dell'utente sia nella lista degli invitati:
+    // controllo che la mail dell'utente sia nella lista degli invitati:
+    if(userMail === listaInvitati[i]){
+        console.log("benvenuto");
+    }
+
+   
+};
+
 
 
 
