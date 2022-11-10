0c91999e7b03595423cfd57e204136cc866fb020
RominaMalimban <romina.lyn@hotmail.it>
2022-11-10 14:36:08 +0100

    creazione prompt e lista email invitati

---
+++ mail/js/script.js
@@ -0,0 +1,18 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// chiedo all'utente la sua mail:
+let userMail = prompt("Per favore, inserisci la tua mail:")
+console.log("email utente:", userMail);
+
+// creo lista delle mail degli invitati:
+let listaInvitati = ["romina@gmail.com", "pincopallo@gmail.com", "pippo@gmail.com"];
+
+
+// controllo che la mail dell'utente sia nella lista degli invitati:
+
+
+
+// stampo messaggio per esito:
